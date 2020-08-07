71dbce34f4323b7de8824067598c68a73f975b2e
Matheus Schreiner Homrich da Silva <matheus-homrich@NTCNOTE81.ntconsult.net>
2020-08-07 09:29:26 -0300

    ampliacao rep

---
+++ CursoJava/Data.java
@@ -0,0 +1,9 @@
+package data;
+import java.util.Date;
+public class Data{
+    public static void main(String args[]){
+        Date data = new Date();
+        System.out.println(data);
+
+    }
+}
---
+++ CursoJava/Teste.java
@@ -0,0 +1,6 @@
+package teste;
+public class Teste {
+    public static void main(String args[]){
+        System.out.println("Oi");
+    }
+}
